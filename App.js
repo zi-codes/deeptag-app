2dae4603819a9fe93b9f9afe27b4a04631c27e5c
Student <ziwei.polaris@gmail.com>
2019-08-28 15:29:51 +0100

    upload button working

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Main from "./components/Main";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Main></Main>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
